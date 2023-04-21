13696bab7b890a3493a6e0c5489b79728abe60d7
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2023-04-22 00:40:43 +0700

    test upload

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@aiti/styles/globals.css'
+import "@aiti/styles/globals.css";
+import "@aiti/styles/css/vendor/bootstrap.min.css";
+import "@aiti/styles/css/vendor/font-awesome.css";
+import "@aiti/styles/css/vendor/slick.css";
+import "@aiti/styles/css/vendor/slick-theme.css";
+import "@aiti/styles/css/vendor/magnific-popup.css";
+import "@aiti/styles/css/vendor/green-audio-player.min.css";
+import "@aiti/styles/css/app.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
