718fbbcf063b70671c41b8e6d0af9f4262e08a63
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2023-04-24 23:49:30 +0700

    next config minify

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  ignoreDuringBuilds: true,
   compress: true,
   images: {
     formats: ["image/avif", "image/webp"],
+  },
+  compiler: {
+    styledComponents: {
+      minify: true,
+    },
   },
 };
 
